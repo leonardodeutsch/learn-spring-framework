c1058a0a07a899e0e9872a442599b752ef819386
Leo <deutschleonardo@gmail.com>
2023-11-13 15:01:53 -0600

    Creating new beans and beans out of existing beans.

--- src/main/java/com/leonardodeutsch/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/leonardodeutsch/learnspringframework/App02HelloWorldSpring.java
@@ -8,7 +8,11 @@ public class App02HelloWorldSpring {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
         System.out.println(context.getBean("name"));
-
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean(Location.class));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
 
     }
 }
--- src/main/java/com/leonardodeutsch/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/leonardodeutsch/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,39 @@ package com.leonardodeutsch.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age, Location location) {};
+record Location (String city, String state) {};
+
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name() {
         return "Leo";
+    }
+
+    @Bean
+    public int age() {
+        return 20;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Leo", 21, new Location("Houston", "TX"));
+    }
+
+    @Bean(name = "location2")
+    public Location location() {
+        return new Location("Spring", "TX");
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), location());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Location location2) {
+        return new Person(name, age, location2);
     }
 
 }
