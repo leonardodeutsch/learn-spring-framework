fdf9bfe457bfc1910676d54d6106d41959c231de
Leo <deutschleonardo@gmail.com>
2023-11-13 14:10:01 -0600

    Second Iteration: Loosely coupled GameRunner

--- src/main/java/com/leonardodeutsch/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/leonardodeutsch/learnspringframework/AppGamingBasicJava.java
@@ -1,16 +1,19 @@
 package com.leonardodeutsch.learnspringframework;
 
+import com.leonardodeutsch.learnspringframework.game.GameRunner;
 import com.leonardodeutsch.learnspringframework.game.MarioGame;
 import com.leonardodeutsch.learnspringframework.game.SuperContraGame;
+import com.leonardodeutsch.learnspringframework.game.PacmanGame;
 
 public class AppGamingBasicJava {
 
     public static void main(String[] args) {
 
-        var marioGame = new MarioGame();
-        var superContraGame = new SuperContraGame();
+        //var game = new MarioGame();
+        //var game = new SuperContraGame();
+        var game = new PacmanGame();
 
-        var gameRunner = new GameRunner(superContraGame);
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
 }
---
+++ src/main/java/com/leonardodeutsch/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,16 @@
+package com.leonardodeutsch.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole {
+    public void up() {
+        System.out.println("Move up");
+    }
+    public void down() {
+        System.out.println("Move down");
+    }
+    public void left() {
+        System.out.println("Move left");
+    }
+    public void right() {
+        System.out.println("Move Right");
+    }
+}
